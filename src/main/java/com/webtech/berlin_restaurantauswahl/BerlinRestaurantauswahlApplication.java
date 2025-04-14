1816c3d72625666c2a63ea424b69b37ed2f42836
Edith-Claire Thon <Edith-Claire.Thon@Student.HTW-Berlin.de>
2025-04-14 13:03:08 +0200

    Spring Initializr entpackt

---
+++ src/main/java/com/webtech/berlin_restaurantauswahl/BerlinRestaurantauswahlApplication.java
@@ -0,0 +1,13 @@
+package com.webtech.berlin_restaurantauswahl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerlinRestaurantauswahlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerlinRestaurantauswahlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webtech/berlin_restaurantauswahl/BerlinRestaurantauswahlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtech.berlin_restaurantauswahl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerlinRestaurantauswahlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
