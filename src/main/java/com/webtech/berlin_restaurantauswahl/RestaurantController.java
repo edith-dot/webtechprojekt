3ea73f5705503be99f90f6cbdcdd8ebdcb7f2920
Edith-Claire Thon <Edith-Claire.Thon@Student.HTW-Berlin.de>
2025-04-30 19:04:45 +0200

    CrossOrigin

--- src/main/java/com/webtech/berlin_restaurantauswahl/RestaurantController.java
+++ src/main/java/com/webtech/berlin_restaurantauswahl/RestaurantController.java
@@ -2,16 +2,19 @@ package com.webtech.berlin_restaurantauswahl;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class RestaurantController {
 
     @GetMapping("/berliner-restaurantauswahl")
     public List<Restaurant> getRestaurants() {
         return List.of(
-                new Restaurant("Restaurant Beispiel", "Beispielstr. 187", "Mo-So, 10-20 Uhr", "Kebab")
+                new Restaurant ("Restaurant Eins", "Beispielstr. 187", "Mo-So, 10-20 Uhr", "Kebab"),
+                new Restaurant("Restaurant Zwei", "Beispielstr. 69", "Mo-So, 10-20 Uhr", "Italienisch")
         );
     }
 }
